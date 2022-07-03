03c3a6666bf61a59b867725cfb4ff49f1696103d
Rob-Martinez <86384534+Rob-Martinez@users.noreply.github.com>
2022-07-02 22:28:34 -0500

    change app.js to functional component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,103 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-class App extends Component {
-  constructor() {
-    super();
 
-    this.state = { 
-      monsters: [],
-      searchField: '',
-      };
-    }
 
-    // This lifecycle mount grabs the data asynchronously from the api then is ran after the initial render.
-    componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/users')
+
+
+const App = () => {
+
+
+  const [searchField, setSearchField] = useState(''); // [] array destructing is [value, setValue] and useState(''); is empty to start and changes as info is intered in the searchbox
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(resp => resp.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      },
-      () => {console.log(users)}
-      ))
-    }
+      .then((users) => setMonsters(users));
+  }, []);
 
-    onSearchChange = (event) => {
-      const searchField = event.target.value.toLocaleLowerCase();
-      this.setState(() => {
-        return { searchField }
-      })
-    }
+  useEffect(() => {
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
 
-    render() {
-      const { monsters, searchField } = this.state;
-      const { onSearchChange } = this;
-      const filteredMonsters = monsters.filter((monster) => {
-        return monster.name.toLocaleLowerCase().includes(searchField)
-      })
+    setFilteredMonsters(filteredMonsters);
+  }, [monsters, searchField])
 
-
-      return (
-      <div className="App">
-        <SearchBox
-          onChangeHandler={onSearchChange} 
-          classname='monsters-search-box' 
-          placeholder='Search Monster Friends'
-        />
-        <CardList monsters={ filteredMonsters }/>
-      </div>
-    );
-    }
-
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   }
+
+  return(
+    <div>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        classname='monsters-search-box' 
+        placeholder='Search Monster Friends'
+      />
+      <CardList monsters={ filteredMonsters }/>
+
+    </div>
+  );
+  
+}
+
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = { 
+//       monsters: [],
+//       searchField: '',
+//       };
+//     }
+
+//     // This lifecycle mount grabs the data asynchronously from the api then is ran after the initial render.
+//     componentDidMount() {
+//       fetch('https://jsonplaceholder.typicode.com/users')
+//       .then(resp => resp.json())
+//       .then((users) => this.setState(() => {
+//         return {monsters: users}
+//       },
+//       () => {console.log(users)}
+//       ))
+//     }
+
+//     onSearchChange = (event) => {
+//       const searchField = event.target.value.toLocaleLowerCase();
+//       this.setState(() => {
+//         return { searchField }
+//       })
+//     }
+
+//     render() {
+//       const { monsters, searchField } = this.state;
+//       const { onSearchChange } = this;
+//       const filteredMonsters = monsters.filter((monster) => {
+//         return monster.name.toLocaleLowerCase().includes(searchField)
+//       })
+
+
+//       return (
+//       <div className="App">
+//         <SearchBox
+//           onChangeHandler={onSearchChange} 
+//           classname='monsters-search-box' 
+//           placeholder='Search Monster Friends'
+//         />
+//         <CardList monsters={ filteredMonsters }/>
+//       </div>
+//     );
+//     }
+
+//   }
 
 export default App;
